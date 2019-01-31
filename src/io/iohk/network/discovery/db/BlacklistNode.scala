286c96d62b61e322c241fca57b89aa7c8f280b08
Jeremy <jeremy.townson@gmail.com>
2019-01-31 15:36:43 +0100

    Remove io.iohk.network and create io.iohk.scalanet.

--- src/io/iohk/network/discovery/db/BlacklistNode.scala
+++
@@ -1,7 +0,0 @@
-package io.iohk.network.discovery.db
-
-import java.time.Instant
-
-import io.iohk.network.NodeInfo
-
-case class BlacklistNode(nodeInfo: NodeInfo, blacklistSince: Instant, blacklistUntil: Instant)
