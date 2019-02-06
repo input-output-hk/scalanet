61fd7076728974c08ef1473115566c1afa996a50
Shailesh Patil <shaileshp@gmail.com>
2019-02-06 10:37:11 +0000

    Added new Simple Peer Group

--- src/io/iohk/scalanet/peergroup/SimplePeerGroup.scala
+++ src/io/iohk/scalanet/peergroup/SimplePeerGroup.scala
@@ -1,6 +1,5 @@
 package io.iohk.scalanet.peergroup
 
-
 import java.nio.ByteBuffer
 
 import io.iohk.scalanet.messagestream.MessageStream
@@ -9,8 +8,8 @@ import io.iohk.scalanet.peergroup.SimplePeerGroup.Config
 
 import scala.language.higherKinds
 
-class SimplePeerGroup[A,F[_],AA](val config: Config,underLinePeerGroup:PeerGroup[AA,F])(implicit liftF: Lift[F])
-  extends NonTerminalPeerGroup[A,F,AA](underLinePeerGroup) {
+class SimplePeerGroup[A, F[_], AA](val config: Config, underLinePeerGroup: PeerGroup[AA, F])(implicit liftF: Lift[F])
+    extends NonTerminalPeerGroup[A, F, AA](underLinePeerGroup) {
   override def sendMessage(address: A, message: ByteBuffer): F[Unit] = ???
 
   override def shutdown(): F[Unit] = ???
@@ -18,7 +17,7 @@ class SimplePeerGroup[A,F[_],AA](val config: Config,underLinePeerGroup:PeerGroup
   override def messageStream(): MessageStream[ByteBuffer] = ???
 }
 
-object SimplePeerGroup{
+object SimplePeerGroup {
 
   case class Config()
 
