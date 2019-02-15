6e1d46cd992b21c4f938cf8874c3e851cbfa9cee
Shailesh Patil <shaileshp@gmail.com>
2019-02-15 14:09:48 +0000

    test added

--- src/io/iohk/scalanet/peergroup/SimplePeerGroup.scala
+++ src/io/iohk/scalanet/peergroup/SimplePeerGroup.scala
@@ -27,11 +27,14 @@ class SimplePeerGroup[A: PartialCodec, F[_], AA: PartialCodec](
 
   private val routingTable: mutable.Map[A, AA] = new ConcurrentHashMap[A, AA]().asScala
 
+
   private val msgPartialCodec: PartialCodec[PeerMessage[A, AA]] = PartialCodec[PeerMessage[A, AA]]
   private val msgCodec = Codec.heapCodec(msgPartialCodec)
 
   override val processAddress: A = config.processAddress
   override val messageStream: MessageStream[ByteBuffer] = underLyingPeerGroup.messageStream()
+
+
 
   private val handle: (Int, ByteBuffer) => Unit = (nextIndex, byteBuffer) => {
     val messageE: Either[Failure, DecodeResult[PeerMessage[A, AA]]] = msgPartialCodec.decode(nextIndex, byteBuffer)
@@ -52,10 +55,11 @@ class SimplePeerGroup[A: PartialCodec, F[_], AA: PartialCodec](
     })
   }
 
-  private val decoderWrappers: Map[String, (Int, ByteBuffer) => Unit] = Map(msgPartialCodec.typeCode -> handle)
+  private val decoderWrappers: Map[String, (Int, ByteBuffer) => Unit] =
+    Map(msgPartialCodec.typeCode -> handle)
 
   messageStream.foreach { b =>
-    println(s"GOT A MESSAGE. DECODING IT.")
+    println(s"GOT A MESSAGE. DECODING IT." +b.toString)
     Codec.decodeFrame(decoderWrappers, 0, b)
   }
 
@@ -90,10 +94,6 @@ class SimplePeerGroup[A: PartialCodec, F[_], AA: PartialCodec](
           msgCodec.encode(EnrolMe(config.processAddress, underLyingPeerGroup.processAddress))
         )
     })
-
-    //    routingTable.filterKeys(_ != processAddress).foreach {
-    //      case (a,aa) => underLinePeerGroup.sendMessage(aa,EnrolMe(processAddress,underLinePeerGroup.processAddress))
-    //    }
     this
   }
 }
@@ -122,63 +122,5 @@ object SimplePeerGroup {
       heapCodec[EnrolMe[String, InetSocketAddress]].decode(byteBuffer)
 
   }
-  //  object Enrolled {
-  //    implicit def toByteBuffer(implicit enrolled: Enrolled[String, InetSocketAddress]) =
-  //      heapCodec[Enrolled[String, InetSocketAddress]].encode(enrolled)
-  //    implicit def toEnrolled(implicit bf: ByteBuffer) = heapCodec[Enrolled[String, InetSocketAddress]].decode(bf)
-  //
-  //  }
 
 }
-
-//abstract class MessageSpec[Content <: AnyRef](implicit contentCt: ClassTag[Content]) {
-//  val contentClass: Class[_] = contentCt.runtimeClass
-//  final val messageName: String = """Spec\$$""".r.replaceAllIn(getClass.getSimpleName, "")
-//
-//  def maxLength: Int
-//
-//  def deserializeData(bytes: Array[Byte]): Try[Content]
-//
-//  def serializeData(data: Content): Array[Byte]
-//
-//  override def toString: String = s"MessageSpec($messageName)"
-//}
-//object PeersSpec extends MessageSpec[KnownPeers] {
-//  private val AddressLength = 4
-//  private val PortLength    = 4
-//  private val DataLength    = 4
-//
-//  override val messageCode: Message.MessageCode = 2: Byte
-//
-//  override val maxLength: Int = DataLength + 1000 * (AddressLength + PortLength)
-//
-//  override def deserializeData(bytes: Array[Byte]): Try[KnownPeers] = Try {
-//    val lengthBytes = util.Arrays.copyOfRange(bytes, 0, DataLength)
-//    val length      = Ints.fromByteArray(lengthBytes)
-//
-//    assert(bytes.length == DataLength + (length * (AddressLength + PortLength)), "Data does not match length")
-//
-//    KnownPeers((0 until length).map { i =>
-//      val position     = lengthBytes.length + (i * (AddressLength + PortLength))
-//      val addressBytes = util.Arrays.copyOfRange(bytes, position, position + AddressLength)
-//      val address      = InetAddress.getByAddress(addressBytes)
-//      val portBytes    = util.Arrays.copyOfRange(bytes, position + AddressLength, position + AddressLength + PortLength)
-//      new InetSocketAddress(address, Ints.fromByteArray(portBytes))
-//    })
-//  }
-//
-//  override def serializeData(peers: KnownPeers): Array[Byte] = {
-//    val length      = peers.peers.size
-//    val lengthBytes = Ints.toByteArray(length)
-//
-//    val xs = for {
-//      inetAddress <- peers.peers
-//      address     <- Option(inetAddress.getAddress)
-//    } yield (address.getAddress, inetAddress.getPort)
-//
-//    xs.foldLeft(lengthBytes) {
-//      case (bs, (peerAddress, peerPort)) =>
-//        Bytes.concat(bs, peerAddress, Ints.toByteArray(peerPort))
-//    }
-//  }
-//}
--- src/io/iohk/scalanet/test/peergroup/SimplePeerGroupSpec.scala
+++ src/io/iohk/scalanet/test/peergroup/SimplePeerGroupSpec.scala
@@ -1,6 +1,9 @@
 package io.iohk.scalanet.peergroup
 
 import java.net.InetSocketAddress
+import java.nio.ByteBuffer
+
+import io.iohk.decco.Codec.heapCodec
 //import java.nio.ByteBuffer
 
 import io.iohk.decco.auto._
@@ -12,38 +15,55 @@ import io.iohk.scalanet.peergroup.future._
 import monix.execution.Scheduler.Implicits.global
 //import org.mockito.Mockito.{verify, when}
 import org.scalatest.FlatSpec
-//import org.scalatest.Matchers._
-//import org.scalatest.concurrent.ScalaFutures._
+import org.scalatest.Matchers._
+import org.scalatest.concurrent.ScalaFutures._
 //import org.scalatest.mockito.MockitoSugar._
-
+import NetUtils._
 import scala.concurrent.Future
-
+import org.scalatest.EitherValues._
 class SimplePeerGroupSpec extends FlatSpec {
 
-  it should "work" in NetUtils.withTwoRandomTCPPeerGroups { (a, b) =>
-    val bob = new SimplePeerGroup(SimplePeerGroup.Config("Bob", Map.empty[String, InetSocketAddress]), b)
+//  it should "work" in withTwoRandomTCPPeerGroups { (a, b) =>
+//    val bob = new SimplePeerGroup(SimplePeerGroup.Config("Bob", Map.empty[String, InetSocketAddress]), b)
+//
+//    val alice = new SimplePeerGroup(SimplePeerGroup.Config("Alice", Map("Bob" -> b.processAddress)), a)
+//
+//    Thread.sleep(5000)
+//  }
 
-    val alice = new SimplePeerGroup(SimplePeerGroup.Config("Alice", Map("Bob" -> b.processAddress)), a)
+  it should "send a message to it self" in withTwoRandomTCPPeerGroups {(a,b) =>
+    val bob = new SimplePeerGroup(SimplePeerGroup.Config("Bob", Map.empty[String, InetSocketAddress]), a)
 
-    Thread.sleep(5000)
+   // val alice = new SimplePeerGroup(SimplePeerGroup.Config("Alice", Map("Bob" -> a.processAddress)), b)
+    val message = "HI!! Bob"
+    val codec = heapCodec[String]
+    val bytes: ByteBuffer = codec.encode(message)
+    val messageReceivedF = bob.messageStream.head()
+
+     bob.sendMessage("Bob",bytes)
+    val messageReceived =codec.decode(messageReceivedF.futureValue)
+
+    messageReceived.right.value shouldBe message
+
   }
 
-//  it should "send a message to a self SimplePeerGroup" in {
-//    val underlyingPeerGroup = mock[PeerGroup[String, Future]]
-//    when(underlyingPeerGroup.processAddress).thenReturn("underlying")
-//    val messageStream = mock[MessageStream[ByteBuffer]]
-//    when(underlyingPeerGroup.messageStream()).thenReturn(messageStream)
-//    val message = ByteBuffer.wrap(randomBytes(1024))
-//
-//    val simplePeerGroup = createSimplePeerGroup(underlyingPeerGroup, Map.empty[String, String])
-//    val nodeAddress = "A"
-//    when(underlyingPeerGroup.sendMessage(nodeAddress, message)).thenReturn(Future(()))
-//
-//    simplePeerGroup.sendMessage(nodeAddress, message)
-//    verify(underlyingPeerGroup).sendMessage("underlying", message)
-//
-//  }
-//
+  it should "send a message to a other peer of SimplePeerGroup" in withTwoRandomTCPPeerGroups {(a,b) =>
+    val bob = new SimplePeerGroup(SimplePeerGroup.Config("Bob", Map.empty[String, InetSocketAddress]), a)
+
+    val alice = new SimplePeerGroup(SimplePeerGroup.Config("Alice", Map("Bob" -> a.processAddress)), b)
+    val message = "HI!! Bob"
+    val codec = heapCodec[String]
+    val bytes: ByteBuffer = codec.encode(message)
+    val messageReceivedF = bob.messageStream.head()
+
+    alice.sendMessage("Bob",bytes)
+
+    val messageReceived =codec.decode(messageReceivedF.futureValue)
+
+    messageReceived.right.value shouldBe message
+
+  }
+
 //  it should "send a message to a other peer in SimplePeerGroup" in {
 //    val underlyingPeerGroup = mock[PeerGroup[String, Future]]
 //    when(underlyingPeerGroup.processAddress).thenReturn("underlying")
