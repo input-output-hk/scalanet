6da90608da95e42ceaef52e1b9bbef1d9a42e377
Shailesh Patil <shaileshp@gmail.com>
2019-02-18 13:51:53 +0000

    refactoring of udp group test moved to netutils

--- src/io/iohk/scalanet/test/peergroup/UDPPeerGroupSpec.scala
+++ src/io/iohk/scalanet/test/peergroup/UDPPeerGroupSpec.scala
@@ -4,18 +4,17 @@ import java.net.BindException
 import java.nio.ByteBuffer
 import java.nio.charset.StandardCharsets.UTF_8
 
-import io.iohk.scalanet.NetUtils
-import io.iohk.scalanet.NetUtils.{aRandomAddress, withUDPAddressInUse}
+
 import io.iohk.scalanet.peergroup.UDPPeerGroup.Config
 import io.iohk.scalanet.peergroup.future._
 import org.scalatest.EitherValues._
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers._
-import org.scalatest.concurrent.ScalaFutures._
-
+import io.iohk.scalanet.NetUtils._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.concurrent.duration._
+import org.scalatest.concurrent.ScalaFutures._
 
 class UDPPeerGroupSpec extends FlatSpec {
 
@@ -29,39 +28,28 @@ class UDPPeerGroupSpec extends FlatSpec {
 
     pg1.sendMessage(pg2.config.bindAddress, ByteBuffer.wrap(b))
 
-    NetUtils.toArray(value.futureValue) shouldBe b
+    toArray(value.futureValue) shouldBe b
   }
 
   it should "shutdown cleanly" in {
-    val pg1 = randomUDPPeerGroup()
-    NetUtils.isListeningUDP(pg1.config.bindAddress) shouldBe true
+    val pg1 = randomUDPPeerGroup
+    isListeningUDP(pg1.config.bindAddress) shouldBe true
 
     pg1.shutdown().futureValue
 
-    NetUtils.isListeningUDP(pg1.config.bindAddress) shouldBe false
+    isListeningUDP(pg1.config.bindAddress) shouldBe false
   }
 
   it should "support a throws create method" in withUDPAddressInUse { address =>
-    NetUtils.isListeningUDP(address) shouldBe true
+   isListeningUDP(address) shouldBe true
     val exception = the[IllegalStateException] thrownBy UDPPeerGroup.createOrThrow(Config(address))
     exception.getCause shouldBe a[BindException]
   }
 
   it should "support an Either create method" in withUDPAddressInUse { address =>
-    NetUtils.isListeningUDP(address) shouldBe true
+    isListeningUDP(address) shouldBe true
     UDPPeerGroup.create(Config(address)).left.value.cause shouldBe a[BindException]
   }
 
-  private def randomUDPPeerGroup() = new UDPPeerGroup(Config(aRandomAddress()))
 
-  private def withTwoRandomUDPPeerGroups(testCode: (UDPPeerGroup[Future], UDPPeerGroup[Future]) => Any): Unit = {
-    val pg1 = randomUDPPeerGroup()
-    val pg2 = randomUDPPeerGroup()
-    try {
-      testCode(pg1, pg2)
-    } finally {
-      pg1.shutdown()
-      pg2.shutdown()
-    }
-  }
 }
