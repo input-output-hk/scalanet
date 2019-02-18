5cc03bb211819f4a9e655a71e2fedb11326023f8
Jeremy Townson <jeremy.townson@gmail.com>
2019-02-18 17:28:30 +0100

    Tidy up MessageChannel a bit.

--- src/io/iohk/scalanet/peergroup/TCPPeerGroup.scala
+++ src/io/iohk/scalanet/peergroup/TCPPeerGroup.scala
@@ -46,43 +46,11 @@ class TCPPeerGroup[F[_]](val config: Config)(implicit liftF: Lift[F])
     .childOption[java.lang.Boolean](ChannelOption.SO_KEEPALIVE, true)
     .bind(config.bindAddress)
     .syncUninterruptibly()
-  override def createMessageChannel[MessageType: PartialCodec](): MessageChannel[InetSocketAddress, MessageType, F] =
-    new TerminalGroupMessageChannel(this)
 
-//  override def createMessageChannel[MessageType: PartialCodec](): MessageChannel[InetSocketAddress, MessageType, F] = {
-//    new MessageChannel[InetSocketAddress, MessageType, F] {
-//
-//      private val ev = PartialCodec[MessageType]
-//      private val codec = Codec.heapCodec
-//
-//      private val subscribers = new Subscribers[MessageType]()
-//
-//      private val handle: (Int, ByteBuffer) => Unit = (nextIndex, byteBuffer) => {
-//        val messageE: Either[Failure, DecodeResult[MessageType]] = ev.decode(nextIndex, byteBuffer)
-//        messageE match {
-//          case Left(Failure) =>
-//            println(s"OH DEAR, DECODING FAILED")
-//          case Right(decodeResult) =>
-//            println(s"${config.processAddress}: Got a successful decode $decodeResult. Notifying subscribers")
-//            subscribers.notify(decodeResult.decoded)
-//        }
-//      }
-//
-//      private val decoderWrappers: Map[String, (Int, ByteBuffer) => Unit] =
-//        Map(ev.typeCode -> handle)
-//
-//      messageStream.foreach { b =>
-//        println(s"$processAddress: GOT A MESSAGE. DECODING IT." + b.toString)
-//        Codec.decodeFrame(decoderWrappers, 0, b)
-//      }
-//
-//      override val inboundMessages = new MonixMessageStream(subscribers.monixMessageStream)
-//
-//      override def sendMessage(address: InetSocketAddress, message: MessageType): F[Unit] = {
-//        TCPPeerGroup.this.sendMessage(address, codec.encode(message))
-//      }
-//    }
-//  }
+  private val decoderTable = new DecoderTable()
+
+  override def createMessageChannel[MessageType: PartialCodec](): MessageChannel[InetSocketAddress, MessageType, F] =
+    new MessageChannel(this, decoderTable)
 
   override def sendMessage(address: InetSocketAddress, message: ByteBuffer): F[Unit] = {
     val send: Task[Unit] = Task {
@@ -93,7 +61,6 @@ class TCPPeerGroup[F[_]](val config: Config)(implicit liftF: Lift[F])
             .writeAndFlush(Unpooled.wrappedBuffer(message))
             .addListener((_: ChannelFuture) => ctx.channel().close())
         }
-
       }
 
       clientBootstrap
@@ -129,7 +96,7 @@ class TCPPeerGroup[F[_]](val config: Config)(implicit liftF: Lift[F])
     }
   }
 
-  override val messageStream: MessageStream[ByteBuffer] = new MonixMessageStream(subscribers.monixMessageStream)
+  override val messageStream: MessageStream[ByteBuffer] = new MonixMessageStream(subscribers.monixMessageStream).map(_.duplicate())
 
   override val processAddress: InetSocketAddress = config.processAddress
 }
