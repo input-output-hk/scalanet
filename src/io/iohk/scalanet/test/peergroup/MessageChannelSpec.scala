7d02366e42525b02f3652450075ee5f7a6769715
Jeremy Townson <jeremy.townson@gmail.com>
2019-02-21 09:41:52 +0100

    WIP get async messaging test working.

--- src/io/iohk/scalanet/test/peergroup/MessageChannelSpec.scala
+++ src/io/iohk/scalanet/test/peergroup/MessageChannelSpec.scala
@@ -3,16 +3,15 @@ package io.iohk.scalanet.peergroup
 import java.nio.ByteBuffer
 
 import io.iohk.decco.Codec.heapCodec
-import io.iohk.decco.PartialCodec
 import org.scalatest.FlatSpec
-import org.mockito.Mockito.{when}
+import org.mockito.Mockito.when
 import org.scalatest.mockito.MockitoSugar._
 import org.scalatest.concurrent.ScalaFutures._
 import org.scalatest.Matchers._
 
 import scala.concurrent.Future
 import io.iohk.decco.auto._
-import io.iohk.scalanet.messagestream.MonixMessageStream
+import org.scalatest.concurrent.ScalaFutures
 class MessageChannelSpec extends FlatSpec {
 
   behavior of "MessageChannel"
@@ -33,25 +32,24 @@ class MessageChannelSpec extends FlatSpec {
 //    verify(peerGroup).sendMessage(address,bytes)
 //
 //  }
-
+  import scala.concurrent.duration._
+  implicit val patienceConfig = ScalaFutures.PatienceConfig(timeout = 1 second, interval = 100 millis)
   it should "able to consume message stream" in {
     val message = "String"
-    val address = "bob"
     val codec = heapCodec[String]
     val bytes: ByteBuffer = codec.encode(message)
-    val pc = PartialCodec[String]
-
     val peerGroup = mock[PeerGroup[String, Future]]
-    val decoderTable = new DecoderTable()
-    //val function = mock[(Int,ByteBuffer) => Unit]
-    //decoderTable.put(pc.typeCode,function)
+    val subscribers = new Subscribers[ByteBuffer]()
 
-    when(peerGroup.messageStream()).thenReturn(MonixMessageStream.fromIterable(List(bytes)))
-    val messageChannel = new MessageChannel[String, String, Future](peerGroup, decoderTable)
-    Thread.sleep(2000)
-    val receivedMessageF = messageChannel.inboundMessages.head().futureValue
-    println("********" + receivedMessageF)
-    receivedMessageF shouldBe message
+    when(peerGroup.messageStream()).thenReturn(subscribers.messageStream)
+    when(peerGroup.processAddress).thenReturn("A")
+
+    val messageChannel = new MessageChannel[String, String, Future](peerGroup, new DecoderTable())
+    val receivedMessageF = messageChannel.inboundMessages.head()
+
+    subscribers.notify(bytes)
+
+    receivedMessageF.futureValue shouldBe message
   }
 
 }
