17e805ca8a7426a23326c373e6570c54698c6d88
Shailesh Patil <shaileshp@gmail.com>
2019-03-22 16:42:57 +0000

    Multicast SimplePeerGroup

--- src/io/iohk/scalanet/peergroup/package.scala
+++ src/io/iohk/scalanet/peergroup/package.scala
@@ -3,10 +3,10 @@ package io.iohk.scalanet
 import scala.collection.mutable
 
 package object peergroup {
-  implicit class MapExtension[K,V](map: Map[K,List[V]]) {
-    def swap: Map[V, K] =  map.flatMap{case (k, vs) => vs.map(v => (v, k)).toMap}
+  implicit class MapExtension[K, V](map: Map[K, List[V]]) {
+    def swap: Map[V, K] = map.flatMap { case (k, vs) => vs.map(v => (v, k)).toMap }
   }
-  implicit class MutableMapExtension[K,V](map: mutable.Map[K,List[V]]) {
-    def swap: mutable.Map[V, K] =  map.flatMap{case (k, vs) => vs.map(v => (v, k)).toMap}
+  implicit class MutableMapExtension[K, V](map: mutable.Map[K, List[V]]) {
+    def swap: mutable.Map[V, K] = map.flatMap { case (k, vs) => vs.map(v => (v, k)).toMap }
   }
 }
