eb3c50c61d1a1bf8794ab8516bda8b45c165ff66
Shailesh Patil <shaileshp@gmail.com>
2019-03-28 07:50:54 +0000

    Fixed the issue

--- src/io/iohk/scalanet/test/peergroup/SimplePeerGroupSpec.scala
+++ src/io/iohk/scalanet/test/peergroup/SimplePeerGroupSpec.scala
@@ -12,32 +12,33 @@ import org.scalatest.concurrent.ScalaFutures
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
-//import scala.util.Random
+import scala.util.Random
 
 class SimplePeerGroupSpec extends FlatSpec {
 
   implicit val patienceConfig = ScalaFutures.PatienceConfig(timeout = 1 second, interval = 100 millis)
 
   behavior of "SimplePeerGroup"
-//
-//  it should "send a message to itself" in new SimpleTerminalPeerGroups {
-//    terminalPeerGroups.foreach { terminalGroup =>
-//      withASimplePeerGroup(terminalGroup, "Alice") { alice =>
-//        val message = Random.alphanumeric.take(1044).mkString
-//        val messageReceivedF = alice.messageChannel[String].headL.runToFuture
-//
-//        alice.sendMessage("Alice", message).runToFuture.futureValue
-//
-//        val messageReceived = messageReceivedF.futureValue
-//        messageReceived shouldBe (alice.processAddress, message)
-//      }
-//    }
-//  }
+
+  it should "send a message to itself" in new SimpleTerminalPeerGroups {
+    terminalPeerGroups.foreach { terminalGroup =>
+      withASimplePeerGroup(terminalGroup, "Alice") { alice =>
+        val message = Random.alphanumeric.take(1044).mkString
+        val messageReceivedF = alice.messageChannel[String].headL.runToFuture
+
+        alice.sendMessage("Alice", message).runToFuture.futureValue
+
+        val messageReceived = messageReceivedF.futureValue
+        messageReceived shouldBe (alice.processAddress, message)
+      }
+    }
+  }
 
   it should "send and receive a message to another peer of SimplePeerGroup" in new SimpleTerminalPeerGroups {
     terminalPeerGroups.foreach { terminalGroup =>
       withTwoSimplePeerGroups(
         terminalGroup,
+        List.empty[String],
         "Alice",
         "Bob"
       ) { (alice, bob) =>
@@ -62,30 +63,73 @@ class SimplePeerGroupSpec extends FlatSpec {
     }
   }
 
+  it should "send  a message to another peers MultiCast address of SimplePeerGroup" in new SimpleTerminalPeerGroups {
+    terminalPeerGroups.foreach { terminalGroup =>
+      withTwoSimplePeerGroups(
+        terminalGroup,
+        List("news", "sports"),
+        "Alice",
+        "Bob"
+      ) { (alice, bob) =>
+        val message = "HI!! Alice"
+
+        val messageReceivedF = alice.messageChannel[String].headL.runToFuture
+        bob.sendMessage("Alice", message).runToFuture.futureValue
+        val messageReceived = messageReceivedF.futureValue
+        messageReceived shouldBe (bob.processAddress, message)
+
+        val messageReceivedByBobF = bob.messageChannel[String].headL.runToFuture
+        val aliceMessage = "HI!! Bob"
+        alice.sendMessage("Bob", aliceMessage).runToFuture.futureValue
+        val messageReceivedByBob = messageReceivedByBobF.futureValue
+        messageReceivedByBob shouldBe (alice.processAddress, aliceMessage)
+
+        val messageReceivedByBobNewsF = bob.messageChannel[String].headL.runToFuture
+        val messageNews = "Latest News"
+        alice.sendMessage("news", messageNews).runToFuture.futureValue
+        val messageReceivedByBobNews = messageReceivedByBobNewsF.futureValue
+        messageReceivedByBobNews shouldBe (alice.processAddress, messageNews)
+
+        val messageReceivedByBobSportsF = bob.messageChannel[String].headL.runToFuture
+        val messageSports = "Sports Updates"
+        alice.sendMessage("sports", messageSports).runToFuture.futureValue
+        val messageReceivedByBobSports = messageReceivedByBobSportsF.futureValue
+        messageReceivedByBobSports shouldBe (alice.processAddress, messageSports)
+
+      }
+    }
+  }
+
   trait SimpleTerminalPeerGroups {
-    val terminalPeerGroups = List(TcpTerminalPeerGroup /*, UdpTerminalPeerGroup*/ )
+    val terminalPeerGroups = List(TcpTerminalPeerGroup, UdpTerminalPeerGroup)
   }
 
   private def withASimplePeerGroup(
       underlyingTerminalGroup: SimpleTerminalPeerGroup,
       a: String
   )(testCode: SimplePeerGroup[String, InetSocketAddress] => Any): Unit = {
-    withSimplePeerGroups(underlyingTerminalGroup, a)(groups => testCode(groups(0)))
+    withSimplePeerGroups(underlyingTerminalGroup, a, List.empty[String])(groups => testCode(groups(0)))
   }
 
-  private def withTwoSimplePeerGroups(underlyingTerminalGroup: SimpleTerminalPeerGroup, a: String, b: String)(
+  private def withTwoSimplePeerGroups(
+      underlyingTerminalGroup: SimpleTerminalPeerGroup,
+      multiCastAddresses: List[String],
+      a: String,
+      b: String
+  )(
       testCode: (
           SimplePeerGroup[String, InetSocketAddress],
           SimplePeerGroup[String, InetSocketAddress]
       ) => Any
   ): Unit = {
 
-    withSimplePeerGroups(underlyingTerminalGroup, a, b)(groups => testCode(groups(0), groups(1)))
+    withSimplePeerGroups(underlyingTerminalGroup, a, multiCastAddresses, b)(groups => testCode(groups(0), groups(1)))
   }
 
   private def withSimplePeerGroups(
       underlyingTerminalGroup: SimpleTerminalPeerGroup,
       bootstrapAddress: String,
+      multiCastAddresses: List[String],
       addresses: String*
   )(
       testCode: Seq[SimplePeerGroup[String, InetSocketAddress]] => Any
@@ -104,7 +148,7 @@ class SimplePeerGroupSpec extends FlatSpec {
           new SimplePeerGroup(
             SimplePeerGroup.Config(
               address,
-              List.empty[String],
+              multiCastAddresses,
               Map(bootstrapAddress -> List(bootStrapTerminalGroup.processAddress))
             ),
             randomTerminalPeerGroup(underlyingTerminalGroup)
