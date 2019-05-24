fb312d687fddc187df72cd61cea55c3c1464a32a
Shailesh Patil <shailesh.patil@iohk.io>
2019-05-24 11:05:46 +0100

    reformat

--- src/io/iohk/scalanet/test/peergroup/SimplePeerGroupSpec.scala
+++ src/io/iohk/scalanet/test/peergroup/SimplePeerGroupSpec.scala
@@ -23,7 +23,7 @@ class SimplePeerGroupSpec extends FlatSpec {
   it should "send a message to itself" in new SimpleTerminalPeerGroups {
     terminalPeerGroups.foreach { terminalGroup =>
       withASimplePeerGroup(terminalGroup, "Alice") { alice =>
-        val message = "Hi ALICE"//Random.alphanumeric.take(1044).mkString
+        val message = "Hi ALICE" //Random.alphanumeric.take(1044).mkString
         val aliceReceived = alice.server().mergeMap(_.in).headL.runToFuture
         val aliceClient: Channel[String, String] = alice.client(alice.processAddress).evaluated
         aliceClient.sendMessage(message).runToFuture
