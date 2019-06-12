32ee868ef898f95d6db74fba31d6a88dac6ff945
Ezequiel Postan <ezequiel_postan@hotmail.com>
2019-06-12 16:16:46 -0300

    A-667: Add question
    Test added to discuss behaviour

--- src/io/iohk/scalanet/test/peergroup/UDPPeerGroupSpec.scala
+++ src/io/iohk/scalanet/test/peergroup/UDPPeerGroupSpec.scala
@@ -8,7 +8,6 @@ import scala.concurrent.Future
 import scala.concurrent.duration._
 import io.iohk.decco.auto._
 import monix.execution.Scheduler.Implicits.global
-
 import org.scalatest.concurrent.ScalaFutures._
 import io.iohk.scalanet.TaskValues._
 
@@ -42,5 +41,17 @@ class UDPPeerGroupSpec extends FlatSpec {
     pg1.shutdown().runAsync.futureValue
 
     isListeningUDP(pg1.config.bindAddress) shouldBe false
+  }
+
+  it should "receive a connection" in withTwoRandomUDPPeerGroups[String] { (alice, bob) =>
+    val aliceIncommingConnections = alice.server()
+    aliceIncommingConnections foreach { c =>
+      println(s"Alice: ${c.to} connected to me")
+    }
+    aliceIncommingConnections.subscribe()
+    // if you uncomment the message sending, then this works, is this the intended behaviour? i.e. for the channel not to
+    // be created in the server side until we receive the first message? is this the behaviour with TCP (and other) peer groups?
+    bob.client(alice.processAddress).evaluated //.sendMessage("Hi Alice")
+    aliceIncommingConnections.headL.runAsync.futureValue.to shouldBe bob.processAddress
   }
 }
