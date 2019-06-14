e668fee1272597c2cefd237d27a8b45d555ffdc0
Shailesh Patil <shailesh.patil@iohk.io>
2019-06-14 14:46:55 +0100

    Addressed PR review comments

--- src/io/iohk/scalanet/test/peergroup/InetPeerGroupUtilsSpec.scala
+++ src/io/iohk/scalanet/test/peergroup/InetPeerGroupUtilsSpec.scala
@@ -1,12 +1,13 @@
 package io.iohk.scalanet.peergroup
 
-import java.net.{Inet4Address, Inet6Address, InetAddress, InetSocketAddress}
+import java.net.{InetAddress, InetSocketAddress}
 
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.concurrent.ScalaFutures._
 import org.scalatest.{BeforeAndAfterAll, FlatSpec}
 import org.scalatest.Matchers._
-
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.prop.Tables.Table
 import scala.concurrent.duration._
 class InetPeerGroupUtilsSpec extends FlatSpec with BeforeAndAfterAll {
 
@@ -14,28 +15,27 @@ class InetPeerGroupUtilsSpec extends FlatSpec with BeforeAndAfterAll {
 
   behavior of "getChannelId"
 
-  it should "create a channelId for ip6address" in {
-    val ipv6Addrr1 = "4B0C:0:0:0:880C:99A8:4B0:4411"
-    val ipv6Addrr2 = "4B0C:0:0:0:880C:99A8:4B0:4411"
-    val remoteInet6Address = InetAddress.getByName(ipv6Addrr1).asInstanceOf[Inet6Address]
-    val localInet6Address = InetAddress.getByName(ipv6Addrr2).asInstanceOf[Inet6Address]
-    val remoteAddress = new InetSocketAddress(remoteInet6Address, 11111)
-    val localAddress = new InetSocketAddress(localInet6Address, 22222)
+  val values = Table(
+    ("ipAddress1", "ipAddress2", "port1", "port2"),
+    ("4B0C:0:0:0:880C:99A8:4B0:4411", "4B0D:0:0:0:880C:99A8:4B0:4411", 1111, 222),
+    ("2001:0db8:85a3:0000:0000:8a2e:0370:7334", "2002:0db8:85a3:0000:0000:8a2e:0370:7334", 1111, 222),
+    ("::1", "::1", 128, 128),
+    ("localhost", "localhost", 1111, 2222),
+    ("127.0.0.1", "127.0.0.1", 1111, 2222),
+    ("127.0.0.0", "127.0.0.0", 0, 0)
+  )
 
-    InetPeerGroupUtils.getChannelId(remoteAddress, localAddress).hashCode shouldBe a[Integer]
+  it should "create a channelId for all ip6address and ip4Address" in {
 
-  }
+    TableDrivenPropertyChecks.forAll(values) { (ipAddress1, ipAddress2, port1, port2) =>
+      val remoteInet6Address = InetAddress.getByName(ipAddress1)
+      val localInet6Address = InetAddress.getByName(ipAddress2)
+      val remoteAddress = new InetSocketAddress(remoteInet6Address, port1)
+      val localAddress = new InetSocketAddress(localInet6Address, port2)
 
-  it should "create a channelId for ip4address" in {
-    val ipv4Addrr1 = "localhost"
-    val ipv4Addrr2 = "localhost"
+      InetPeerGroupUtils.getChannelId(remoteAddress, localAddress).hashCode shouldBe a[Integer]
 
-    val remoteInet6Address = InetAddress.getByName(ipv4Addrr1).asInstanceOf[Inet4Address]
-    val localInet6Address = InetAddress.getByName(ipv4Addrr2).asInstanceOf[Inet4Address]
-    val remoteAddress = new InetSocketAddress(remoteInet6Address, 11111)
-    val localAddress = new InetSocketAddress(localInet6Address, 22222)
-
-    InetPeerGroupUtils.getChannelId(remoteAddress, localAddress).hashCode shouldBe a[Integer]
+    }
 
   }
 
