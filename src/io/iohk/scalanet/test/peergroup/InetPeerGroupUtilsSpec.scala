72cc049a60be9466afc397504bcf3928eb192282
Jeremy Townson <jeremy.townson@gmail.com>
2019-06-18 09:20:09 +0200

    Add a testcase to check channelId is different for distinct addresses.

--- src/io/iohk/scalanet/test/peergroup/InetPeerGroupUtilsSpec.scala
+++ src/io/iohk/scalanet/test/peergroup/InetPeerGroupUtilsSpec.scala
@@ -1,42 +1,42 @@
 package io.iohk.scalanet.peergroup
 
-import java.net.{InetAddress, InetSocketAddress}
+import java.net.InetSocketAddress
 
-import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.concurrent.ScalaFutures._
-import org.scalatest.{BeforeAndAfterAll, FlatSpec}
+import org.scalatest.FlatSpec
 import org.scalatest.Matchers._
 import org.scalatest.prop.TableDrivenPropertyChecks._
 import org.scalatest.prop.Tables.Table
-import scala.concurrent.duration._
-class InetPeerGroupUtilsSpec extends FlatSpec with BeforeAndAfterAll {
 
-  implicit val patienceConfig: ScalaFutures.PatienceConfig = PatienceConfig(5 seconds)
+import InetPeerGroupUtils.getChannelId
+
+class InetPeerGroupUtilsSpec extends FlatSpec {
 
   behavior of "getChannelId"
 
   val values = Table(
-    ("ipAddress1", "ipAddress2", "port1", "port2"),
-    ("4B0C:0:0:0:880C:99A8:4B0:4411", "4B0D:0:0:0:880C:99A8:4B0:4411", 1111, 222),
-    ("2001:0db8:85a3:0000:0000:8a2e:0370:7334", "2002:0db8:85a3:0000:0000:8a2e:0370:7334", 1111, 222),
-    ("::1", "::1", 128, 128),
-    ("localhost", "localhost", 1111, 2222),
-    ("127.0.0.1", "127.0.0.1", 1111, 2222),
-    ("127.0.0.0", "127.0.0.0", 0, 0)
+    ("address0", "address1"),
+    (addr("4B0C:0:0:0:880C:99A8:4B0:4411", 1111), addr("4B0D:0:0:0:880C:99A8:4B0:4411", 2222)),
+    (addr("2001:0db8:85a3:0000:0000:8a2e:0370:7334", 1111), addr("2002:0db8:85a3:0000:0000:8a2e:0370:7334", 2222)),
+    (addr("::1", 128), addr("::1", 128)),
+    (addr("localhost", 1111), addr("localhost", 2222)),
+    (addr("127.0.0.1", 1111), addr("127.0.0.1", 2222)),
+    (addr("127.0.0.0", 0), addr("127.0.0.0", 0))
   )
 
-  it should "create a channelId for all ip6address and ip4Address" in {
-
-    forAll(values) { (ipAddress1, ipAddress2, port1, port2) =>
-      val remoteInet6Address = InetAddress.getByName(ipAddress1)
-      val localInet6Address = InetAddress.getByName(ipAddress2)
-      val remoteAddress = new InetSocketAddress(remoteInet6Address, port1)
-      val localAddress = new InetSocketAddress(localInet6Address, port2)
-
-      InetPeerGroupUtils.getChannelId(remoteAddress, localAddress).hashCode shouldBe a[Integer]
-
+  it should "create a channelId for IPv6 and IPv4 6addresses" in {
+    forAll(values) { (address0, address1) =>
+      noException should be thrownBy getChannelId(address0, address1)
     }
-
   }
 
+  it should "provide distinct channel ids for distinct address pairs" in {
+    forAll(values) { (address00, address01) =>
+      forAll(values) { (address10, address11) =>
+        if ((address00, address01) != (address10, address11))
+          getChannelId(address00, address01) should not be getChannelId(address10, address11)
+      }
+    }
+  }
+
+  private def addr(host: String, port: Int) = new InetSocketAddress(host, port)
 }
