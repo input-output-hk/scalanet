4385553c5cd5f61964af7152444bc7b16847da6e
Shailesh Patil <shaileshp@gmail.com>
2019-06-18 08:44:48 +0100

    A-761 Fixes a bug in channel id for IPv6.

--- src/io/iohk/scalanet/peergroup/InetPeerGroupUtils.scala
+++ src/io/iohk/scalanet/peergroup/InetPeerGroupUtils.scala
@@ -1,13 +1,15 @@
 package io.iohk.scalanet.peergroup
 
 import java.net.InetSocketAddress
-import java.nio.ByteBuffer
 
 import io.netty.util
 import monix.eval.Task
 import monix.execution.Cancelable
 
 object InetPeerGroupUtils {
+
+  type ChannelId = (InetSocketAddress, InetSocketAddress)
+
   def toTask(f: util.concurrent.Future[_]): Task[Unit] = {
     Task.create[Unit] { (_, cb) =>
       f.addListener(
@@ -17,12 +19,8 @@ object InetPeerGroupUtils {
     }
   }
 
-  def getChannelId(remoteAddress: InetSocketAddress, localAddress: InetSocketAddress): Seq[Byte] = {
-    val b = ByteBuffer.allocate(16)
-    b.put(remoteAddress.getAddress.getAddress)
-    b.putInt(remoteAddress.getPort)
-    b.put(localAddress.getAddress.getAddress)
-    b.putInt(localAddress.getPort)
-    b.array().toIndexedSeq
+  def getChannelId(remoteAddress: InetSocketAddress, localAddress: InetSocketAddress): ChannelId = {
+    (remoteAddress, localAddress)
   }
+
 }
