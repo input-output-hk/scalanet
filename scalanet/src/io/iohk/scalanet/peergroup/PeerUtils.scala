fae7f7cc71c4ebd1031a725e1ff1fb22d554bf17
Ezequiel Postan <ezequiel.postan@iohk.io>
2019-06-27 15:44:10 -0300

    Add InMemoryPeerGroup (#37)
    * Add InMemoryPeerGroup
    
    * Address PR comments

---
+++ scalanet/src/io/iohk/scalanet/peergroup/PeerUtils.scala
@@ -0,0 +1,45 @@
+package io.iohk.scalanet.peergroup
+
+import monix.eval.Task
+import monix.execution.Scheduler
+
+import scala.language.higherKinds
+
+object PeerUtils {
+
+  def apply[A, M, PG[_, _]](implicit tu: PeerUtils[A, M, PG]): PeerUtils[A, M, PG] = tu
+
+  def instance[A, M, PG[_, _]](
+      isLis: PG[A, M] => Boolean,
+      generateRandomPG: () => PG[A, M],
+      shutd: PG[A, M] => Task[Unit],
+      init: PG[A, M] => Task[Unit]
+  ): PeerUtils[A, M, PG] = new PeerUtils[A, M, PG] {
+    override def isListening(peer: PG[A, M]): Boolean = isLis(peer)
+    override def generateRandomPeerGroup(): PG[A, M] = generateRandomPG()
+    override def shutdown(peer: PG[A, M]): Task[Unit] = shutd(peer)
+    override def initialize(peer: PG[A, M]): Task[Unit] = init(peer)
+  }
+}
+
+trait PeerUtils[A, M, PG[_, _]] {
+  def isListening(peer: PG[A, M]): Boolean
+  def generateRandomPeerGroup(): PG[A, M]
+  def shutdown(peer: PG[A, M]): Task[Unit]
+  def initialize(peer: PG[A, M]): Task[Unit]
+
+  def withTwoRandomPeerGroups(
+      testFunction: (PG[A, M], PG[A, M]) => Any
+  )(implicit sc: Scheduler): Unit = {
+    val alice = generateRandomPeerGroup()
+    val bob = generateRandomPeerGroup()
+    initialize(alice).runAsync
+    initialize(bob).runAsync
+    try {
+      testFunction(alice, bob)
+    } finally {
+      shutdown(alice).runAsync
+      shutdown(bob).runAsync
+    }
+  }
+}
