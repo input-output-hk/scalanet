f950f6cd11d98fdb331793909b609b53c7292bfd
Shailesh Patil <shailesh.patil@iohk.io>
2019-07-16 19:12:01 +0100

    pr comments addressed

--- scalanet/src/io/iohk/scalanet/peergroup/ControlEvent.scala
+++ scalanet/src/io/iohk/scalanet/peergroup/ControlEvent.scala
@@ -6,5 +6,5 @@ object ControlEvent {
 
   case object Initialized
 
-  case class InitializationError(message: String, cause: Throwable) extends RuntimeException
+  case class InitializationError(message: String, cause: Throwable) extends RuntimeException(message,cause)
 }
