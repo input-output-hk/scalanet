320bcf6cbe61a12789ec60f2b94bba4f9d88a97f
Shailesh Patil <shailesh.patil@iohk.io>
2019-08-24 21:47:08 +0100

    code cleanup

--- scalanet/src/io/iohk/scalanet/monix_subject/ConnectableSubject.scala
+++
@@ -1,8 +0,0 @@
-package io.iohk.scalanet.monix_subject
-
-import monix.execution.Cancelable
-import monix.reactive.subjects.Subject
-
-abstract class ConnectableSubject[T] extends Subject[T, T] {
-  def connect(): Cancelable
-}
