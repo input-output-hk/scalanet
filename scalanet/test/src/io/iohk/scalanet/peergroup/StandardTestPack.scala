df2c8838833254c863536cd888d4d20f65852dba
Shailesh Patil <shaileshp@gmail.com>
2019-08-27 19:18:10 +0100

    A scalanet missing udp test (#44)
    * missing test and updated the readme
    
    * missing test and updated the readme

--- scalanet/test/src/io/iohk/scalanet/peergroup/StandardTestPack.scala
+++ scalanet/test/src/io/iohk/scalanet/peergroup/StandardTestPack.scala
@@ -33,7 +33,6 @@ object StandardTestPack {
   ): Unit = {
     val alicesMessage = Random.alphanumeric.take(1024).mkString
     val bobsMessage = Random.alphanumeric.take(1024).mkString
-
     bob.server().collectChannelCreated.foreach(channel => channel.sendMessage(bobsMessage).runAsync)
 
     val aliceClient1 = alice.client(bob.processAddress).evaluated
@@ -43,8 +42,11 @@ object StandardTestPack {
     val aliceReceived2 = aliceClient2.in.headL.runAsync
 
     aliceClient1.sendMessage(alicesMessage).runAsync
+    aliceClient2.sendMessage(alicesMessage).runAsync
 
     aliceReceived1.futureValue shouldBe bobsMessage
+    aliceReceived2.futureValue shouldBe bobsMessage
+
     recoverToSucceededIf[IllegalStateException](aliceReceived2)
   }
 
