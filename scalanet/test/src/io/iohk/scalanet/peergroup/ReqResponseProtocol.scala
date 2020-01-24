399af0f4fcbdc335507d5dbeb175e87529457310
KonradStaniec <konrad.staniec@gmail.com>
2020-01-24 17:34:10 +0100

    [PM-1243] Additional concurrent tests

---
+++ scalanet/test/src/io/iohk/scalanet/peergroup/ReqResponseProtocol.scala
@@ -0,0 +1,172 @@
+package io.iohk.scalanet.peergroup
+
+import java.net.InetSocketAddress
+import java.nio.ByteBuffer
+import java.util.UUID
+
+import cats.effect.concurrent.Ref
+import io.iohk.decco.{BufferInstantiator, Codec, CodecContract}
+import io.iohk.scalanet.NetUtils.aRandomAddress
+import io.iohk.scalanet.codec.FramingCodec
+import io.iohk.scalanet.peergroup.InetPeerGroupUtils.ChannelId
+import io.iohk.scalanet.peergroup.ReqResponseProtocol._
+import monix.eval.Task
+import monix.execution.Scheduler
+import monix.reactive.observables.ConnectableObservable
+
+import scala.concurrent.duration._
+import scala.concurrent.duration.FiniteDuration
+
+/**
+  * Simple higher level protocol on top of generic peer group. User is shielded from differnt implementation details like:
+  * channels, observables etc.
+  *
+  * For now used only in testing as:
+  *   - it lacks any error handling
+  *   - it is not entairly thread safe
+  *   - can only handle simple server handler
+  *   - there is not resource cleaning
+  *
+  * @param group transport peer group
+  * @param state currently open client channels
+  * @tparam M the message type.
+  */
+class ReqResponseProtocol[M](
+    group: PeerGroup[InetMultiAddress, MessageEnvelope[M]],
+    state: Ref[Task, Map[ChannelId, Channel[InetMultiAddress, MessageEnvelope[M]]]]
+)(implicit s: Scheduler) {
+
+  private def getChan(
+      to: InetMultiAddress,
+      chId: ChannelId,
+      st: Map[ChannelId, Channel[InetMultiAddress, MessageEnvelope[M]]]
+  ): Task[Channel[InetMultiAddress, MessageEnvelope[M]]] = {
+    // this is not really thread safe.
+    if (st.contains(chId)) {
+      Task.now(st(chId))
+    } else {
+      for {
+        newCh <- group.client(to)
+        // start publishing incoming messages to any subscriber
+        // in normal circumstances we should keep cancellation token to clear up resources
+        canc = newCh.in.connect()
+        // Keep in mind that stream is back pressured for all subscribers so in case of many parallel requests to one client
+        // waiting for response on first request can influence result of second request
+        _ <- state.set(st.updated(chId, newCh))
+      } yield newCh
+    }
+  }
+
+  // It do not closes client channel after each message as, in case of tcp it would be really costly
+  // to create new tcp connection for each message.
+  // it probably should return Task[Either[E, M]]
+  def send(m: M, to: InetMultiAddress, requestDuration: FiniteDuration = 5.seconds): Task[M] = {
+    val chID = (group.processAddress.inetSocketAddress, to.inetSocketAddress)
+    for {
+      st <- state.get
+      ch <- getChan(to, chID, st)
+      randomUuid = UUID.randomUUID()
+      mes = MessageEnvelope(randomUuid, m)
+      resp <- sendMandAwaitForResponse(ch, mes, requestDuration)
+    } yield resp
+  }
+
+  private def sendMandAwaitForResponse(
+      c: Channel[InetMultiAddress, MessageEnvelope[M]],
+      messageToSend: MessageEnvelope[M],
+      timeOutDuration: FiniteDuration
+  ): Task[M] =
+    for {
+      // sending and subsription are done in parallel to not miss response message by chance
+      // alse in case of send failure, any resource will be cleaned
+      result <- Task
+        .parMap2(c.sendMessage(messageToSend), subscribeForResponse(c.in, messageToSend.id))(
+          (_, response) => response.m
+        )
+        .timeout(timeOutDuration)
+    } yield result
+
+  private def subscribeForResponse(source: ConnectableObservable[MessageEnvelope[M]], responseId: UUID) = {
+    source.collect {
+      case response if response.id == responseId => response
+    }.headL
+  }
+
+  def startHandling(requestHandler: M => M) = {
+    group
+      .server()
+      .refCount
+      .collectChannelCreated
+      .mergeMap(channel => channel.in.refCount.map(request => (channel, request)))
+      .foreachL {
+        case (ch, mes) =>
+          ch.sendMessage(MessageEnvelope(mes.id, requestHandler(mes.m))).runAsyncAndForget
+      }
+  }
+
+  def processAddress: InetMultiAddress = group.processAddress
+}
+
+object ReqResponseProtocol {
+  final case class MessageEnvelope[M](id: UUID, m: M)
+
+  object MessageEnvelope {
+    implicit def mCCinstance[M](implicit codecContract: CodecContract[M]): CodecContract[MessageEnvelope[M]] = {
+      import io.iohk.decco.auto._
+      val uud = implicitly[CodecContract[UUID]]
+      // it would be nice to have such combinators for Codec not only  CodeContract
+      // manual coded is needed as auto deriviation does not handle well cases when class has abstract type member here M
+      uud
+        .zip(codecContract)
+        .map(
+          { case (uuid, m) => MessageEnvelope(uuid, m) },
+          fi => (fi.id, fi.m)
+        )
+    }
+  }
+
+  private def buildProtocol[M](
+      group: PeerGroup[InetMultiAddress, MessageEnvelope[M]]
+  )(implicit s: Scheduler): Task[ReqResponseProtocol[M]] = {
+    for {
+      _ <- group.initialize()
+      initState <- Ref.of[Task, Map[ChannelId, Channel[InetMultiAddress, MessageEnvelope[M]]]](Map.empty)
+      prot <- Task.now(new ReqResponseProtocol[M](group, initState))
+    } yield prot
+  }
+
+  sealed abstract class TransportProtocol extends Product with Serializable
+  case object Udp extends TransportProtocol
+  case object Tcp extends TransportProtocol
+
+  def getTcpReqResponseProtocolClient[M](
+      address: InetSocketAddress = aRandomAddress()
+  )(implicit s: Scheduler, b: BufferInstantiator[ByteBuffer], c: CodecContract[M]): Task[ReqResponseProtocol[M]] = {
+    import MessageEnvelope._
+    import io.iohk.decco.auto._
+    val codec = implicitly[Codec[MessageEnvelope[M]]]
+    implicit lazy val framingCodec = new FramingCodec[MessageEnvelope[M]](codec)
+    val pg1 = new TCPPeerGroup[MessageEnvelope[M]](TCPPeerGroup.Config(address))
+    buildProtocol(pg1)
+  }
+
+  def getUdpReqResponseProtocolClient[M](
+      address: InetSocketAddress = aRandomAddress()
+  )(implicit s: Scheduler, b: BufferInstantiator[ByteBuffer], c: CodecContract[M]): Task[ReqResponseProtocol[M]] = {
+    import MessageEnvelope._
+    import io.iohk.decco.auto._
+    val pg1 = new UDPPeerGroup[MessageEnvelope[M]](UDPPeerGroup.Config(address))
+    buildProtocol(pg1)
+  }
+
+  def getReqResponseProtocol[M](
+      transport: TransportProtocol,
+      address: InetSocketAddress = aRandomAddress()
+  )(implicit s: Scheduler, b: BufferInstantiator[ByteBuffer], c: CodecContract[M]): Task[ReqResponseProtocol[M]] = {
+    transport match {
+      case Udp => getUdpReqResponseProtocolClient(address)
+      case Tcp => getTcpReqResponseProtocolClient(address)
+    }
+  }
+
+}
---
+++ scalanet/test/src/io/iohk/scalanet/peergroup/TransportPeerGroupAsyncSpec.scala
@@ -0,0 +1,133 @@
+package io.iohk.scalanet.peergroup
+
+import java.util.concurrent.{Executors, TimeUnit}
+
+import monix.eval.Task
+import monix.execution.Scheduler
+import org.scalatest.{Assertion, AsyncFlatSpec, BeforeAndAfterAll}
+
+import scala.concurrent.{ExecutionContext, Future}
+import org.scalatest.prop.TableDrivenPropertyChecks._
+import io.iohk.decco.BufferInstantiator.global.HeapByteBuffer
+import io.iohk.decco.auto._
+import io.iohk.scalanet.peergroup.ReqResponseProtocol.{Tcp, Udp}
+import org.scalatest.Matchers._
+
+/**
+  *
+  * Spec to test different concurrency scenario on top of our low level peer groups.
+  * FIXME Add more scenarios. Add tls.
+  *
+  */
+class TransportPeerGroupAsyncSpec extends AsyncFlatSpec with BeforeAndAfterAll {
+  val threadPool = Executors.newFixedThreadPool(16)
+  val testContext = ExecutionContext.fromExecutor(threadPool)
+  implicit val scheduler = Scheduler(testContext)
+
+  override def afterAll(): Unit = {
+    threadPool.shutdown()
+    threadPool.awaitTermination(60, TimeUnit.SECONDS)
+  }
+
+  private val rpcs = Table(
+    ("Label", "Transport type"),
+    ("TCP", Tcp),
+    ("UDP", Udp)
+  )
+  forAll(rpcs) { (label, transportType) =>
+    import TransportPeerGroupAsyncSpec._
+
+    s"Request response on top of ${label}" should "exchange messages between clients sequentially" in taskTestCase {
+      for {
+        client1 <- ReqResponseProtocol.getReqResponseProtocol[String](transportType)
+        client2 <- ReqResponseProtocol.getReqResponseProtocol[String](transportType)
+        - <- Task.parZip2(
+          client1.startHandling(echoDoubleHandler).startAndForget,
+          client2.startHandling(echoDoubleHandler).startAndForget
+        )
+        resp <- client1.send(msg1, client2.processAddress)
+        resp2 <- client2.send(msg3, client1.processAddress)
+        resp1 <- client1.send(msg2, client2.processAddress)
+        resp3 <- client2.send(msg3 ++ msg1, client1.processAddress)
+      } yield {
+        resp shouldEqual msg1 ++ msg1
+        resp1 shouldEqual msg2 ++ msg2
+        resp2 shouldEqual msg3 ++ msg3
+        resp3 shouldEqual (msg3 ++ msg1) ++ (msg3 ++ msg1)
+      }
+    }
+
+    s"Request response on top of ${label}" should "exchange messages between clients concurrently" in taskTestCase {
+      for {
+        client1 <- ReqResponseProtocol.getReqResponseProtocol[Int](transportType)
+        client2 <- ReqResponseProtocol.getReqResponseProtocol[Int](transportType)
+        client3 <- ReqResponseProtocol.getReqResponseProtocol[Int](transportType)
+        - <- Task.parZip3(
+          client1.startHandling(doublingHandler).startAndForget,
+          client2.startHandling(doublingHandler).startAndForget,
+          client3.startHandling(doublingHandler).startAndForget
+        )
+        responses <- Task.parZip3(
+          client1.send(i, client2.processAddress),
+          client2.send(j, client3.processAddress),
+          client3.send(k, client1.processAddress)
+        )
+        (r1, r2, r3) = responses
+        responses <- Task.gather((1 to 4).map { req =>
+          if (req % 2 == 0) {
+            client1.send(req, client3.processAddress)
+          } else {
+            client2.send(req, client3.processAddress)
+          }
+        })
+      } yield {
+        r1 shouldEqual 2 * i
+        r2 shouldEqual 2 * j
+        r3 shouldEqual 2 * k
+        responses shouldEqual (1 to 4).map(2 * _)
+      }
+    }
+
+    s"Request response on top of ${label}" should "exchange messages between clients concurrently for multiple messages" in taskTestCase {
+      val client1Numbers = (1 to 20).toList
+      val client2Numbers = (10 to 30).toList
+      val client3Numbers = (20 to 40).toList
+      for {
+        client1 <- ReqResponseProtocol.getTcpReqResponseProtocolClient[Int]()
+        client2 <- ReqResponseProtocol.getTcpReqResponseProtocolClient[Int]()
+        client3 <- ReqResponseProtocol.getTcpReqResponseProtocolClient[Int]()
+        - <- Task.parZip3(
+          client1.startHandling(doublingHandler).startAndForget,
+          client2.startHandling(doublingHandler).startAndForget,
+          client3.startHandling(doublingHandler).startAndForget
+        )
+        responses <- Task.parZip3(
+          Task.sequence(client1Numbers.map(num => client1.send(num, client3.processAddress))),
+          Task.sequence(client2Numbers.map(num => client2.send(num, client3.processAddress))),
+          Task.sequence(client3Numbers.map(num => client3.send(num, client1.processAddress)))
+        )
+        (resp1, resp2, resp3) = responses
+
+      } yield {
+        resp1 shouldEqual client1Numbers.map(2 * _)
+        resp2 shouldEqual client2Numbers.map(2 * _)
+        resp3 shouldEqual client3Numbers.map(2 * _)
+      }
+    }
+  }
+}
+
+object TransportPeerGroupAsyncSpec {
+  def taskTestCase(t: => Task[Assertion])(implicit s: Scheduler): Future[Assertion] =
+    t.runToFuture
+
+  val echoDoubleHandler: String => String = s => s ++ s
+  val msg1 = "Hello"
+  val msg2 = "Server"
+  val msg3 = "This is the way"
+
+  val doublingHandler: Int => Int = i => i * 2
+  val i = 1
+  val j = 2
+  val k = 3
+}
