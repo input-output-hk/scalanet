9789fecc814e6d2453fe90a84e66608710d51392
MaximilianoRey <mrey@atixlabs.com>
2020-01-29 16:30:22 -0300

    add reference

--- scalanet/src/io/iohk/scalanet/crypto/package.scala
+++ scalanet/src/io/iohk/scalanet/crypto/package.scala
@@ -1,3 +1,5 @@
+//based in mantis cryptography code https://github.com/input-output-hk/mantis/blob/master/src/main/scala/io/iohk/ethereum/crypto/package.scala
+
 package io.iohk.scalanet
 
 import java.math.BigInteger
@@ -8,32 +10,54 @@ import org.spongycastle.asn1.x9.X9ECParameters
 import org.spongycastle.crypto.AsymmetricCipherKeyPair
 import org.spongycastle.crypto.digests.SHA256Digest
 import org.spongycastle.crypto.generators.ECKeyPairGenerator
-import org.spongycastle.crypto.params.{ECDomainParameters, ECKeyGenerationParameters, ECPrivateKeyParameters, ECPublicKeyParameters}
+import org.spongycastle.crypto.params.{
+  ECDomainParameters,
+  ECKeyGenerationParameters,
+  ECPrivateKeyParameters,
+  ECPublicKeyParameters
+}
 import org.spongycastle.crypto.signers.{ECDSASigner, HMacDSAKCalculator}
 
-package  object crypto {
+package object crypto {
 
   val curveParams: X9ECParameters = SECNamedCurves.getByName("secp256k1")
-  val curve: ECDomainParameters = new ECDomainParameters(curveParams.getCurve, curveParams.getG, curveParams.getN, curveParams.getH)
+  val curve: ECDomainParameters = new ECDomainParameters(
+    curveParams.getCurve,
+    curveParams.getG,
+    curveParams.getN,
+    curveParams.getH
+  )
 
-  def recuperateEncodedKey(key:Array[Byte] ): ECPublicKeyParameters ={
-    new ECPublicKeyParameters(curve.getCurve.decodePoint(key),curve)
+  def recuperateEncodedKey(key: Array[Byte]): ECPublicKeyParameters = {
+    new ECPublicKeyParameters(curve.getCurve.decodePoint(key), curve)
   }
-  def keyPairToByteArrays(keyPair: AsymmetricCipherKeyPair): (Array[Byte], Array[Byte]) = {
-    val prvKey = keyPair.getPrivate.asInstanceOf[ECPrivateKeyParameters].getD.toByteArray
-    val pubKey = keyPair.getPublic.asInstanceOf[ECPublicKeyParameters].getQ.getEncoded(false).tail
+  def keyPairToByteArrays(
+    keyPair: AsymmetricCipherKeyPair
+  ): (Array[Byte], Array[Byte]) = {
+    val prvKey =
+      keyPair.getPrivate.asInstanceOf[ECPrivateKeyParameters].getD.toByteArray
+    val pubKey = keyPair.getPublic
+      .asInstanceOf[ECPublicKeyParameters]
+      .getQ
+      .getEncoded(false)
+      .tail
     (prvKey, pubKey)
   }
 
-  def encodeKey(key: ECPublicKeyParameters):Array[Byte] = {
-   key.getQ.getEncoded(true)
+  def encodeKey(key: ECPublicKeyParameters): Array[Byte] = {
+    key.getQ.getEncoded(true)
   }
 
-  def generateKeyPair(secureRandom: SecureRandom): (ECPrivateKeyParameters,ECPublicKeyParameters) = {
+  def generateKeyPair(
+    secureRandom: SecureRandom
+  ): (ECPrivateKeyParameters, ECPublicKeyParameters) = {
     val generator = new ECKeyPairGenerator
     generator.init(new ECKeyGenerationParameters(curve, secureRandom))
     val res = generator.generateKeyPair()
-    (res.getPrivate.asInstanceOf[ECPrivateKeyParameters],res.getPublic.asInstanceOf[ECPublicKeyParameters])
+    (
+      res.getPrivate.asInstanceOf[ECPrivateKeyParameters],
+      res.getPublic.asInstanceOf[ECPublicKeyParameters]
+    )
   }
 
   private def canonicalise(s: BigInteger): BigInteger = {
@@ -42,17 +66,21 @@ package  object crypto {
     else s
   }
 
-  def sign(data:Array[Byte],privateKey: ECPrivateKeyParameters):(BigInteger,BigInteger) = {
+  def sign(data: Array[Byte],
+           privateKey: ECPrivateKeyParameters): (BigInteger, BigInteger) = {
     val signer = new ECDSASigner(new HMacDSAKCalculator(new SHA256Digest))
     signer.init(true, privateKey)
     val components = signer.generateSignature(data)
-    (components(0),canonicalise(components(1)))
+    (components(0), canonicalise(components(1)))
   }
 
-  def verify(data:Array[Byte],r:BigInteger,s:BigInteger,publicKey:ECPublicKeyParameters): Boolean = {
+  def verify(data: Array[Byte],
+             r: BigInteger,
+             s: BigInteger,
+             publicKey: ECPublicKeyParameters): Boolean = {
     val signer = new ECDSASigner(new HMacDSAKCalculator(new SHA256Digest))
     signer.init(false, publicKey)
-    signer.verifySignature(data,r,s)
+    signer.verifySignature(data, r, s)
   }
 
 }
