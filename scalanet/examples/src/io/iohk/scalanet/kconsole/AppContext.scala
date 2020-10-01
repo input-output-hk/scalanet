a2949896151e1b2596cef897a331a383310bb515
Akosh Farkash <akosh.farkash@iohk.io>
2020-10-01 14:15:57 +0100

    ETCM-169: Update Scalanet examples to use Resource.

--- scalanet/examples/src/io/iohk/scalanet/kconsole/AppContext.scala
+++ scalanet/examples/src/io/iohk/scalanet/kconsole/AppContext.scala
@@ -1,37 +1,26 @@
 package io.iohk.scalanet.kconsole
 
-import io.iohk.scalanet.peergroup.{InetMultiAddress, PeerGroup}
-import io.iohk.scalanet.peergroup.udp.DynamicUDPPeerGroup
+import cats.effect.Resource
+import io.iohk.scalanet.codec.DefaultCodecs._
+import io.iohk.scalanet.kademlia.codec.DefaultCodecs._
 import io.iohk.scalanet.kademlia.KNetwork.KNetworkScalanetImpl
 import io.iohk.scalanet.kademlia.{KMessage, KRouter}
+import io.iohk.scalanet.peergroup.{InetMultiAddress}
+import io.iohk.scalanet.peergroup.udp.DynamicUDPPeerGroup
 import monix.execution.Scheduler
+import monix.eval.Task
+import scodec.codecs.implicits._
 
-class AppContext(nodeConfig: KRouter.Config[InetMultiAddress])(implicit scheduler: Scheduler) {
-  import scodec.codecs.implicits._
-  import io.iohk.scalanet.codec.DefaultCodecs._
-  import io.iohk.scalanet.kademlia.codec.DefaultCodecs._
+object AppContext {
 
-  val kRouter: KRouter[InetMultiAddress] = {
-
-    try {
-      val routingConfig =
-        DynamicUDPPeerGroup.Config(
-          nodeConfig.nodeRecord.routingAddress.inetSocketAddress
-        )
-      val routingPeerGroup = PeerGroup.createOrThrow(
-        new DynamicUDPPeerGroup[KMessage[InetMultiAddress]](routingConfig),
-        routingConfig
-      )
-      val kNetwork =
-        new KNetworkScalanetImpl[InetMultiAddress](routingPeerGroup)
-
-      KRouter.startRouterWithServerSeq(nodeConfig, kNetwork).runSyncUnsafe()
-    } catch {
-      case e: Exception =>
-        System.err.println(
-          s"Exiting due to initialization error: $e, ${e.getCause}"
-        )
-        throw e
-    }
+  def apply(
+      nodeConfig: KRouter.Config[InetMultiAddress]
+  )(implicit scheduler: Scheduler): Resource[Task, KRouter[InetMultiAddress]] = {
+    val routingConfig = DynamicUDPPeerGroup.Config(nodeConfig.nodeRecord.routingAddress.inetSocketAddress)
+    for {
+      routingPeerGroup <- DynamicUDPPeerGroup[KMessage[InetMultiAddress]](routingConfig)
+      kNetwork = new KNetworkScalanetImpl[InetMultiAddress](routingPeerGroup)
+      kRouter <- Resource.liftF(KRouter.startRouterWithServerSeq(nodeConfig, kNetwork))
+    } yield kRouter
   }
 }
