a5b4c354a8e2cb8c60d99e0a34562d65553a1be1
Akosh Farkash <akosh.farkash@iohk.io>
2020-10-05 09:40:02 +0100

    ETCM-162: Tests for StaticUDPPeerGroup port handling.

--- scalanet/test/src/io/iohk/scalanet/peergroup/udp/StaticUDPPeerGroupSpec.scala
+++ scalanet/test/src/io/iohk/scalanet/peergroup/udp/StaticUDPPeerGroupSpec.scala
@@ -1,14 +1,17 @@
 package io.iohk.scalanet.peergroup.udp
 
 import cats.effect.Resource
+import cats.implicits._
 import java.net.InetSocketAddress
 import monix.execution.Scheduler
 import monix.eval.Task
 import scodec.Codec
 import io.iohk.scalanet.peergroup.PeerGroup
 import io.iohk.scalanet.peergroup.InetMultiAddress
+import scodec.codecs.implicits._
 
 class StaticUDPPeerGroupSpec extends UDPPeerGroupSpec("StaticUDPPeerGroup") {
+
   override def initUdpPeerGroup[M](
       address: InetSocketAddress
   )(implicit s: Scheduler, c: Codec[M]): Resource[Task, UDPPeerGroupSpec.TestGroup[M]] = {
@@ -22,6 +25,17 @@ class StaticUDPPeerGroupSpec extends UDPPeerGroupSpec("StaticUDPPeerGroup") {
     }
   }
 
-  it should "use the server port for reach client it creates" in (pending)
+  it should "use the server port when it opens client channels" in {
+    List
+      .fill(2)(initUdpPeerGroup[String]())
+      .sequence
+      .use {
+        case List(pg1, pg2) =>
+          ???
+      }
+      .runSyncUnsafe()
+  }
+
   it should "replicate incoming messages to all channels connected to the remote address" in (pending)
+  it should "re-emit a server event if a closed channel is re-activated" in (pending)
 }
