a66aa048aaa6d76f25d7f92e0382733657972379
Akosh Farkash <akosh.farkash@iohk.io>
2020-10-08 11:35:56 +0100

    ETCM-170: Try to upgrade scalafmt

--- install-scalafmt.sh
+++ install-scalafmt.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ./coursier bootstrap org.scalameta:scalafmt-cli_2.12:2.7.4 \
   -f \
-  -r bintray:scalameta/maven \
-  -o ./scalafmt --standalone --main org.scalafmt.cli.Cli
+  -r sonatype:snapshots --main org.scalafmt.cli.Cli \
+  --standalone \
+  -o scalafmt
