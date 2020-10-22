b8b28f40662efb332db3f8a083382222cdc1dbe3
Akosh Farkash <akosh.farkash@iohk.io>
2020-10-22 11:21:42 +0100

    ETCM-240: List driven test for InetAddressOps.

--- scalanet/test/src/io/iohk/scalanet/peergroup/InetAddressOpsSpec.scala
+++ scalanet/test/src/io/iohk/scalanet/peergroup/InetAddressOpsSpec.scala
@@ -4,37 +4,45 @@ import java.net.InetAddress
 
 import org.scalatest._
 
-class InetAddressOpsSpec extends FlatSpec with Matchers {
+class InetAddressOpsSpec extends FlatSpec with Matchers with Inspectors {
   import InetAddressOps._
 
-  behavior of "isUnspecified"
+  case class TestCase(ip: String, isUnspecified: Boolean = false, isSpecial: Boolean = false, isLAN: Boolean = false)
 
-  it should "return true for IPv4 unspecified" in {
-    InetAddress.getByName("0.0.0.0").isUnspecified shouldBe true
-  }
-  it should "return true for IPv6 unspecified" in {
-    InetAddress.getByName("0:0:0:0:0:0:0:0").isUnspecified shouldBe true
-  }
+  val cases = List(
+    TestCase("0.0.0.0", isUnspecified = true, isLAN = true),
+    TestCase("0.0.0.1", isLAN = true),
+    TestCase("0:0:0:0:0:0:0:0", isUnspecified = true),
+    TestCase("127.0.0.1", isLAN = true),
+    TestCase("::1", isLAN = true),
+    TestCase("192.168.1.2", isLAN = true),
+    TestCase("192.175.47"),
+    TestCase("192.175.48.0", isSpecial = true),
+    TestCase("192.175.48.127", isSpecial = true),
+    TestCase("192.175.48.255", isSpecial = true),
+    TestCase("192.175.49"),
+    TestCase("255.255.255.255", isSpecial = true),
+    TestCase("2001:4:112::", isSpecial = true),
+    TestCase("140.82.121.4")
+  )
 
-  behavior of "isSpecial"
+  behavior of "InetAddressOps"
 
-  it should "return true for IPv4 specials" in {
-    InetAddress.getByName("192.88.99.0").isSpecial shouldBe true
-  }
-  it should "return true for IPv6 specials" in {
-    InetAddress.getByName("2001:4:112::").isSpecial shouldBe true
-  }
-  it should "return false for loopback" in {
-    InetAddress.getByName("127.0.0.1").isSpecial shouldBe false
-    InetAddress.getByName("::1").isSpecial shouldBe false
-  }
-
-  behavior of "isLAN"
-
-  it should "return true for IPv4 loopback" in {
-    InetAddress.getByName("127.0.0.1").isLAN shouldBe true
-  }
-  it should "return true for IPv6 loopback" in {
-    InetAddress.getByName("::1").isLAN shouldBe true
+  it should "correctly calculate each flag" in {
+    forAll(cases) {
+      case TestCase(ip, isUnspecified, isSpecial, isLAN) =>
+        withClue(ip) {
+          val addr = InetAddress.getByName(ip)
+          withClue("isUnspecified") {
+            addr.isUnspecified shouldBe isUnspecified
+          }
+          withClue("isSpecial") {
+            addr.isSpecial shouldBe isSpecial
+          }
+          withClue("isLAN") {
+            addr.isLAN shouldBe isLAN
+          }
+        }
+    }
   }
 }
